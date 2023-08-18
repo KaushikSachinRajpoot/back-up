9b191b11ecb295ac184e26ef4daa2fd2cd6661c4
Sagar Roy <sagar.r@celestialsys.com>
2023-08-18 09:40:07 +0530

    backend change

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-// const pool = require('./sever/db');
-const { execute } = require('@getvim/execute'); 
+const { execute } = require('@getvim/execute');
 const dotenv = require('dotenv').config();
 
 const app = express();
@@ -8,123 +7,127 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-
 const Pool = require('pg').Pool;
 
-const pool  = new Pool({
-    user: 'postgres',
-    password: 'Cel@123451234',
-    host: 'localhost',
-    port: 5432,
-    database: 'perntodo'
-});
+const baseDbconfig = {
+  user: 'postgres',
+  password: '1234',
+  host: 'localhost',
+  port: 5432,
+  database: 'testabase'
+};
 
-// create to do here
-app.post('/todos', async (req, res)=>{
-try {
-const {description} = req.body;
-    const newTodo = await pool.query(
-        "INSERT INTO todo (description) VALUES($1) RETURNING * " , [description]
-    );
-res.json(newTodo)    
-} catch (error) {
-    console.log(error.massage);
-    
-}
-})
+const versionDbconfig = {
+  user: 'postgres',
+  password: '1234',
+  host: 'localhost',
+  port: 5432,
+  // database: 'version_1'
+};
 
-
-
-// Backup function
-// const takePGBackup = () => {
-   
-
-
-// const backupCommand = `pg_dump -U ${pool.user} -h ${pool.host} -p ${pool.port} -f ${backupFile} -d ${pool.database}`;
-    // const date = new Date();
-    // const today = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    // const backupCommand = `pg_dump -U ${pool.user} -h ${pool.host} -p ${pool.port} -f ${backupFile} -d ${pool.database}`;
+const pool1 = new Pool(baseDbconfig);
+// const pool2 = new Pool(versionDbconfig)
+app.post('/todos', async (req, res) => {
+  const {dbname} = req.body;
+  const newVersionDbconfig = {...versionDbconfig, database: dbname}
+  const pool2 = new Pool(newVersionDbconfig);
+  const client2 = await pool2.connect()
   
-    // function takePGBackup (aut) { 
-    //     console.log(aut , 'auth is here');
-    //     const date = new Date();
-    //     const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-    //     const backupFile = `pg-backup-${today} 1_${}.sql`;
-    //     const backupCommand = `pg_dump -U postgres -h localhost -p 5432 -f ${backupFile} -d perntodo`;
-    //     // console.log("execute here", execute(backupCommand));
-    //     execute(backupCommand).then(async() => {
-    //     console.log(`Backup created successfully`); })
-    //     .catch((err) => { console.log(err);
-    //     });
-    //  };
-    // // }
-//  takePGBackup()
-
-  // app.post('/backup', async (req, res) => {
-  //   const {version} = req.body;
-  //   console.log(version);
-  //   function takePGBackup () { 
-  //       const date = new Date();
-  //       const today = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-  //       const backupFile = `pg-backup-${today}-1_${version}.sql`;
-  //       const backupCommand = `pg_dump -U postgres -h localhost -p 5432 -f ${backupFile} -d perntodo`;
-  //       execute(backupCommand).then(async() => {
-  //       console.log(`Backup created successfully`); })
-  //       .catch((err) => { console.log(err);
-  //       });
-  //    };
-  //   takePGBackup();
-  //   res.json({ message: 'Backup process initiated' });
-  // });
-
-// get all todo here
-
-
-app.get('/todos', async (req, res)=>{
-    try {
-    const allTodo = await pool.query('SELECT * FROM todo')
-    res.json(allTodo);        
-    } catch (error) {
-        
-    }
+  try {
+    const table = await client2.query("SELECT * from emp");
+    res.json(table.rows);
+  } catch (error) {
+    console.log('table error', error);
+  }finally{
+    client2.release();
+  }
 })
 
-app.post('/database', async (req, res)=>{
-    async function fetchAndDisplayDatabases() {
-        try {
-        //   const result = await pool.query('SELECT datname FROM pg_database WHERE datistemplate = false;');
-const result = await pool.query("SELECT datname FROM pg_database WHERE datistemplate = false")
-          const databases = result.rows.map(row => row.datname);
-          res.json({data: databases})
-          console.log('List of databases:', databases);
-        } catch (error) {
-          console.error('Error fetching databases:', error);
-        }
+app.post('/insert', async (req, res) => {
+  const client1 = await pool1.connect();
+  // try {
+  //   const {  } = req.body;
+  //   const result = await client1.query(
+  //     "INSERT INTO users (email, password) VALUES ($1, $2, $3)",
+  //     [id, name, pos])
+  //     res.json(result.rows);
+  // } catch (error) {
+  //   res.status(500).send(error);
+  // }
+  try {
+    const { id, name, pos } = req.body;
+    const insertedRow = await client1.query(
+      "INSERT INTO emp (id, name, pos) VALUES ($1, $2, $3)",
+      [id, name, pos]
+    );
+    res.json(insertedRow.rows)
+  } catch (error) {
+    console.log(error.massage);
+  }
+   finally {
+    client1.release();
+  }
+})
+
+
+
+const fetchAndDisplayDatabases = async(req,res) => {
+  const client1 = await pool1.connect();
+  try {
+    const result = await client1.query("SELECT datname FROM pg_database WHERE datistemplate = false")
+    const databases = result.rows.map(row => row.datname);
+    console.log('List of databases:', databases);
+    let versionNumberArray = []
+    let highestVersion = 0;
+    databases.filter((item) => {
+      if ((item.slice(0, 8)) === "version_") {
+        versionNumberArray.push((item.slice(8)));
       }
-      
-      fetchAndDisplayDatabases();
+    })
+    const sortedVersionNumberArray = versionNumberArray.sort((a, b) => {
+      return b - a;
+    });
+    if(sortedVersionNumberArray[0]){highestVersion = parseInt(sortedVersionNumberArray[0]);}
+    res.json({ data: databases, highestVersion })
+  } catch (error) {
+    console.error('Error fetching databases:', error);
+  }finally{
+    client1.release();
+  }
+}
+app.post('/database', async (req, res) => {
+  fetchAndDisplayDatabases(req,res);
 })
-
-
-
-// const client = new Client(clientConfig);
 
 app.post('/createDatabase', async (req, res) => {
-    const {version} = req.body;
-    try {
-      const templateDb = 'perntodo';
-      const newDb = `version_${version}`;
-      await pool.query(`CREATE DATABASE ${newDb} WITH TEMPLATE ${templateDb} OWNER postgres`);
-      res.status(200).json({ message: 'Database created successfully' });
-    } catch (error) {
-      console.error('Error creating database:', error);
-      res.status(500).json({ error: 'An error occurred while creating the database' });
-    }
-  });
+  const { version } = req.body;
+  const client1 = await pool1.connect();
+  try {
+    const templateDb = 'testabase';
+    const newDb = `version_${version}`;
+    await client1.query(`CREATE DATABASE ${newDb} WITH TEMPLATE ${templateDb} OWNER postgres`);
+    const result = await client1.query("SELECT datname FROM pg_database WHERE datistemplate = false")
+    const databases = result.rows.map(row => row.datname);
+    let versionNumberArray = []
+    let highestVersion = 0;
+    databases.filter((item) => {
+      if ((item.slice(0, 8)) === "version_") {
+        versionNumberArray.push((item.slice(8)));
+      }
+    })
+    const sortedVersionNumberArray = versionNumberArray.sort((a, b) => {
+      return b - a;
+    });
+    if(sortedVersionNumberArray[0]){highestVersion = parseInt(sortedVersionNumberArray[0]);}
+    res.status(200).json({ message: 'Database created successfully', highestVersion });
+  } catch (error) {
+    console.error('Error creating database:', error);
+    res.status(500).json({ error: 'An error occurred while creating the database' });
+  }finally{
+    client1.release();
+  }
+});
 
-
-
-
-app.listen(5000, ()=>{
-    console.log("sever is running is on the server 5000");
+app.listen(5000, () => {
+  console.log("sever is running is on the server 5000");
 })
